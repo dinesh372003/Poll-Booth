301982eac7359091300ea7b83778cfc813883741
dinesh372003 <83570475+dinesh372003@users.noreply.github.com>
2021-08-14 23:55:19 +0530

    Update passport.js

--- passport.js
+++ passport.js
@@ -8,26 +8,30 @@ new LocalStrategy({ passReqToCallback: true ,usernameField:"email",passwordField
 (req,email, password, done)=> 
 {
     //Finding Match for User
-    User.findOne({email: email})
+    User.findOne({email:email})
     .then(user=> 
     {
-        if (!user) {
+        if(!user) 
+        {
         return done(null, false, req.flash("error-login","The email is not registered"));
-    }
-
-    bcrypt.compare(password, user.password, (err, isMatch)=> 
-    {
-    if (err) throw err;
-    if (isMatch) 
-    {
-        return done(null, user);
-    } 
-    else 
-    {
-        return done(null, false, req.flash("error-login","Incorrect Password"));
-    }
-    });
-    });
+        }
+        bcrypt.compare(password, user.password, (err, isMatch)=> 
+        {
+            if(err)
+            {
+                console.log(err);
+            }
+            if(isMatch) 
+            {
+                return done(null, user);
+            } 
+            else 
+            {
+                return done(null, false, req.flash("error-login","Incorrect Password"));
+            }
+        });
+    })
+    .catch(err=>console.log(err));
 })
 );
 
